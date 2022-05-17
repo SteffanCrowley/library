50d58273328b4f3d8c382570efe312ae73559cfa
SteffanCrowley <steffancrow@gmail.com>
2022-05-17 08:11:35 -0700

    Make basic DOM manipulation work

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const container = document.querySelector(".container");
 let myLibrary = [];
 
 function Book(title, author, numberofPages, readStatus) {
@@ -12,9 +13,20 @@ function Book(title, author, numberofPages, readStatus) {
   };
 }
 
+//function that uses book constructor to create new book and then adds
+//it to the library array
 function addBookToLibrary(title, author, numberofPages, readStatus) {
   const newBook = new Book(title, author, numberofPages, readStatus);
   myLibrary.push(newBook);
+}
+
+//function that draws the current board by iterating through
+//the current library array
+function submitBook(myLibrary) {
+  for (let i = 0; i < myLibrary.length, i++; ) {
+    let p = document.createElement("p");
+    p.textContent = "party time";
+  }
 }
 
 console.log(myLibrary);
@@ -24,3 +36,7 @@ console.log(myLibrary);
 // console.log(myLibrary);
 
 // console.log(theHobbit.toString());
+
+let div = document.createElement("div");
+div.textContent = "party time";
+container.appendChild(div);
